e4eb6300500b7465efa507f5dfd6aaebefd68518
MaxTsumentsau <max2ba@yandex.ru>
2026-03-04 23:31:45 +0300

    Initial commit

---
+++ src/test/java/com/max2ba/service_discovery/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.max2ba.service_discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
